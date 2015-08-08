665f06d83d194b1dd41b5a4c00b3b9f947284576
mjhassanpur <mjhassanpur@gmail.com>
2015-08-08 15:39:02 -0700

    Add android activity library

--- jokedisplay/src/main/java/com/udacity/gradle/jokedisplay/JokeActivity.java
+++ jokedisplay/src/main/java/com/udacity/gradle/jokedisplay/JokeActivity.java
@@ -1,29 +1,25 @@
-package com.udacity.gradle.builditbigger;
+package com.udacity.gradle.jokedisplay;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-import com.udacity.gradle.jokedisplay.JokeActivity;
-import com.udacity.gradle.jokes.Joker;
+public class JokeActivity extends ActionBarActivity {
 
-public class MainActivity extends ActionBarActivity {
-
-    private Joker joker = new Joker();
+    public static String JOKE_KEY = "Joke key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_joke);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_joke, menu);
         return true;
     }
 
@@ -41,12 +37,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void startJokeActivity(View view){
-        Intent intent = new Intent(this, JokeActivity.class);
-        String joke = joker.getJoke();
-        intent.putExtra(JokeActivity.JOKE_KEY, joke);
-        startActivity(intent);
-    }
-
 }
